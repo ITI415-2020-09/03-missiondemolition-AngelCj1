4722fb4f67ff2f506136fd5dff8d8796858400d2
AngelCj1 <70732152+AngelCj1@users.noreply.github.com>
2020-09-24 13:20:28 -0400

    SlingShot code
    added code to the slingshot c #

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    // fields set in the Unity Inspector pane
+    public GameObject prefabProjectile;
+    public bool _________________________________________;
+    // fields set dynamically
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+
 
     void Awake() {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
     void OnMouseEnter() {
@@ -19,5 +29,17 @@ public class Slingshot : MonoBehaviour {
     void OnMouseExit() {
         print("Slingshot:OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown() {
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Start it at the launchPoint
+        projectile.transform.position = launchPos;
+        // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+
     }
 }
