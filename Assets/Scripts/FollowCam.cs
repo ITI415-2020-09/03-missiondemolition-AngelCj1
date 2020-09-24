3d2a46af3aa0450a9e32b6e489834e986ae0f920
AngelCj1 <70732152+AngelCj1@users.noreply.github.com>
2020-09-24 14:01:45 -0400

    FollowCam
    added the follow cam code

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+    static public FollowCam S; // a FollowCam Singleton
+
+    // fields set in the Unity Inspector pane
+    public bool ___________________________;
+
+    // fields set dynamically
+    public GameObject poi; // The point of interest
+    public float camZ; // The desired Z pos of the camera
+
+    void Awake() {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+    void Update(){
+        // if there's only one line following an if, it doesn't need braces
+        if (poi == null) return; // return if there is no poi
+
+        // Get the position of the poi
+        Vector3 destination = poi.transform.position;
+        // Retain a destination.z of camZ
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+    }
+}
+
